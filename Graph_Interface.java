f862375c84dea39921d3931f530879fe1ece1e95
Gushcha-Vladislav <vladik.kotov1@yandex.by>
2016-06-24 18:02:37 +0300

    Update Graph_Interface.java

--- Graph_Interface.java
+++ Graph_Interface.java
@@ -7,4 +7,10 @@ public interface Graph_Interface {
     void search_algorithm();
     void output_ways();
     void output_graph();
+    public static void main(String[] args) {
+    Graph P=new Graph();
+    P.input_generation();
+    P.search_algorithm();
+    P.output_ways();
+    }
 }
