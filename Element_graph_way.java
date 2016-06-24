cd9ecc2b7f2ec48f7517ff1ed082937db43ffe67
Gushcha-Vladislav <vladik.kotov1@yandex.by>
2016-06-24 15:16:26 +0300

    Add files via upload
    Примерная реализация интерфейса алгоритма . Класс хранящий информацию о одном пути из вершины A в вершину B.

---
+++ Element_graph_way.java
@@ -0,0 +1,10 @@
+package main.java.algoritm;
+
+/**
+ * Created by Влад on 24.06.2016.
+ */
+public class Element_graph_way {
+        int from;
+        int to;
+        int l;
+}
---
+++ Graph_Interface.java
@@ -0,0 +1,12 @@
+package main.java.algoritm;
+
+/**
+ * Created by Влад on 24.06.2016.
+ */
+public interface Graph_Interface {
+    void input_file();
+    void input_generation();
+    void search_algorithm();
+    void output_ways();
+    void output_graph();
+}
