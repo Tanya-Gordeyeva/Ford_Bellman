22534b837e912195aec4aeadfaeca77de23724c2
Tanya-Gordeyeva <tangord@mail.ru>
2016-06-26 19:24:40 +0300

    Update Graph_Interface.java

--- Graph_Interface.java
+++ Graph_Interface.java
@@ -5,12 +5,6 @@ public interface Graph_Interface {
     void input_file();
     void input_generation();
     void search_algorithm();
-    void output_ways();
+   // void output_ways();
     void output_graph();
-    public static void main(String[] args) {
-    Graph P=new Graph();
-    P.input_generation();
-    P.search_algorithm();
-    P.output_ways();
-    }
 }
